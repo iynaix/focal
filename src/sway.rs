3cd55b7ca1a242a6de8085364d95554bc63cfaf9
Lin Xianyi <iynaix@gmail.com>
2024-09-11 15:41:47 +0800

    Sway window nodes are recursive

--- src/sway.rs
+++ src/sway.rs
@@ -42,7 +42,29 @@ pub struct GetTreeWorkspaceNode {
 #[derive(Debug, Deserialize)]
 pub struct GetTreeWindowNode {
     pub rect: Rect,
-    pub visible: bool,
+    pub nodes: Vec<GetTreeWindowNode>,
+    pub visible: Option<bool>,
+}
+
+impl GetTreeWindowNode {
+    /// recursively collects all leaf nodes
+    pub fn leaf_nodes(&self) -> Vec<&Self> {
+        let mut leaf_nodes = Vec::new();
+        self._leaf_nodes(&mut leaf_nodes);
+        leaf_nodes
+    }
+
+    /// helper function for recursion
+    fn _leaf_nodes<'a>(&'a self, leaf_nodes: &mut Vec<&'a Self>) {
+        if self.nodes.is_empty() {
+            leaf_nodes.push(self);
+        } else {
+            // recurse into child nodes
+            for node in &self.nodes {
+                node._leaf_nodes(leaf_nodes);
+            }
+        }
+    }
 }
 
 #[allow(clippy::module_name_repetitions)]
@@ -102,8 +124,10 @@ impl FocalMonitors for SwayMonitors {
             .iter()
             .flat_map(|mon_node| mon_node.nodes.iter())
             .flat_map(|wksp_node| wksp_node.nodes.iter())
+            // win_nodes are recursive
+            .flat_map(|win_node| win_node.leaf_nodes())
             // only want visible windows
-            .filter(|&win_node| win_node.visible)
+            .filter(|&win_node| win_node.visible == Some(true))
             .map(|win_node| {
                 let rect = &win_node.rect;
                 SlurpGeom {
