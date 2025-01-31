7729e17cb5ac9f4f2f8de0d77c3e520d6cb54cd4
Lin Xianyi <iynaix@gmail.com>
2025-01-31 12:51:26 +0800

    Cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -1,67 +1,72 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/default-linux";
     devenv.url = "github:cachix/devenv";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
+    inputs@{
       devenv,
-      # systems,
+      flake-parts,
+      nixpkgs,
+      self,
       ...
-    }@inputs:
-    let
-      forEachSystem =
-        function:
-        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
-          system: function nixpkgs.legacyPackages.${system}
-        );
-    in
-    {
-      devShells = forEachSystem (pkgs: {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              # https://devenv.sh/reference/options/
-              dotenv.disableHint = true;
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ devenv.flakeModule ];
+      systems = import inputs.systems;
 
-              packages = with pkgs; [
-                cargo-edit
-                grim
-                hyprland
-                rofi-wayland
-                slurp
-                sway
-                tesseract
-                hyprpicker
-                wl-clipboard
-                xdg-utils # xdg-open
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells = {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  dotenv.disableHint = true;
+
+                  packages = with pkgs; [
+                    cargo-edit
+                    grim
+                    hyprland
+                    rofi-wayland
+                    slurp
+                    sway
+                    tesseract
+                    hyprpicker
+                    wl-clipboard
+                    xdg-utils # xdg-open
+                  ];
+
+                  languages.rust.enable = true;
+                }
               ];
+            };
+          };
 
-              languages.rust.enable = true;
-            }
-          ];
+          packages = rec {
+            focal = pkgs.callPackage ./package.nix {
+              version =
+                if self ? "shortRev" then
+                  self.shortRev
+                else
+                  nixpkgs.lib.replaceStrings [ "-dirty" ] [ "" ] self.dirtyShortRev;
+            };
+            default = focal;
+            no-ocr = focal.override { ocr = false; };
+            no-waybar = focal.override { focalWaybar = false; };
+            focal-hyprland = focal.override { backend = "hyprland"; };
+            focal-sway = focal.override { backend = "sway"; };
+            focal-image = focal.override { video = false; };
+          };
         };
-      });
-
-      packages = forEachSystem (pkgs: rec {
-        focal = pkgs.callPackage ./package.nix {
-          version =
-            if self ? "shortRev" then
-              self.shortRev
-            else
-              nixpkgs.lib.replaceStrings [ "-dirty" ] [ "" ] self.dirtyShortRev;
-        };
-        default = focal;
-        no-ocr = focal.override { ocr = false; };
-        no-waybar = focal.override { focalWaybar = false; };
-        focal-hyprland = focal.override { backend = "hyprland"; };
-        focal-sway = focal.override { backend = "sway"; };
-        focal-image = focal.override { video = false; };
-      });
     };
+
+  nixConfig = {
+    extra-substituters = [ "https://focal.cachix.org" ];
+    extra-trusted-public-keys = [ "focal.cachix.org-1:/YkOWkXNH2uK7TnskrVMvda8LyCe4iIbMM1sZN2AOXY=" ];
+  };
 }
